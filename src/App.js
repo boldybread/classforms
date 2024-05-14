40d65e1d9127b7ca7284e723d82c3007ad2d082e
Bradley A <138368512+boldybread@users.noreply.github.com>
2024-05-14 10:24:11 +1000

    feat: react app init, social card form and display created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SocialCardForm from './components/SocialCardsForm';
+
+function App() {
+  return (
+    <div className="App">
+      <SocialCardForm />
+    </div>
+  );
+}
+
+export default App;
